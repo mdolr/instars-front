c954ac93679dd0ab4cc5532ddc0c7e42fa32eb77
Maxime Dolores <19026937+mdolr@users.noreply.github.com>
2021-10-29 16:00:47 +0200

    style: add prettier config

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,11 +3,11 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "../components/HelloWorld.vue";
+import { defineComponent } from 'vue';
+import HelloWorld from '../components/HelloWorld.vue';
 
 export default defineComponent({
-  name: "Home",
+  name: 'Home',
 
   components: {
     HelloWorld,
