6c5f6c02565700f1c696e01e828c59f90407b34d
Maxime Dolores <19026937+mdolr@users.noreply.github.com>
2021-11-02 12:20:50 +0100

    test: add layout-checking tests

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -2,17 +2,36 @@
 // https://nightwatchjs.org/guide
 
 module.exports = {
-  'default e2e tests': (browser) => {
+  /*'Desktop layout': (browser) => {
+    browser.init().waitForElementVisible('#app', 5000)
+    .assert.elementPresent('.v-app-bar')
+    .assert.elementNotPresent
+    .end();
+  },*/
+
+  // Set the browser size to desktop
+  // Then check if the v-app-bar element is present
+  // and if the v-bottom-nav element is not present
+  'Desktop layout': (browser) => {
     browser
       .init()
-      .waitForElementVisible('#app')
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js + TypeScript App')
-      .assert.elementCount('img', 1)
+      .setWindowSize(1080, 1400)
+      .waitForElementVisible('#app', 5000)
+      .assert.visible('.v-app-bar')
+      .assert.not.visible('.v-bottom-navigation')
       .end();
   },
 
-  'example e2e test using a custom command': (browser) => {
-    browser.openHomepage().assert.elementPresent('.hello').end();
+  // Set the browser size to mobile
+  // Then check if the v-app-bar element is not present
+  // and if the v-bottom-nav element is present
+  'Mobile layout': (browser) => {
+    browser
+      .init()
+      .setWindowSize(320, 1400)
+      .waitForElementVisible('#app', 5000)
+      .assert.not.visible('.v-app-bar')
+      .assert.visible('.v-bottom-navigation')
+      .end();
   },
 };
