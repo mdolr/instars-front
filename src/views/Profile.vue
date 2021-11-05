a652d60a84124039e3ac594b06dd48fe0f67e858
Maxime Dolores <19026937+mdolr@users.noreply.github.com>
2021-11-05 10:52:26 +0100

    refactor: change the profile page layout

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,23 +1,39 @@
 <template>
   <v-container justify="center" align="center" style="margin-top: 12px">
-    <v-img
-      lazy-src="https://cdn.discordapp.com/attachments/556951502137065518/900806259228422154/drifterFace.PNG"
-      max-height="128px"
-      max-width="128px"
-      src="https://cdn.discordapp.com/attachments/556951502137065518/900806259228422154/drifterFace.PNG"
-      style="border-radius: 50%; height: 128px; width: 128px; border: solid 0.01em black"
-    ></v-img>
-    <h1 style="margin-bottom: -10px">Maxime</h1>
-    <span class="text-grey-darken-3">@mdolr</span>
-    <p style="margin-top: 10px">Description blablabla <br />zdjioazjdoiaz</p>
-
-    <div class="account-stats" style="margin-top: 10px; display: block">
-      <span><v-icon small>mdi-account-group</v-icon> 127 000 Followers</span><br />
-      <span><v-icon x-small>mdi-camera</v-icon> 323 Pictures</span><br />
-      <span class="text-purple"
-        ><v-icon x-large color="purple">mdi-calendar-multiple-check</v-icon> <strong>PERFECT!</strong></span
-      >
+    <div style="margin: auto; width: 100%; max-width: 600px">
+      <div style="display: flex; flex-direction: row; align-items: start; justify-content: center">
+        <div style="display: flex; flex-direction: row; align-items: start">
+          <v-img
+            lazy-src="https://cdn.discordapp.com/attachments/556951502137065518/900806259228422154/drifterFace.PNG"
+            max-height="96px"
+            max-width="96px"
+            src="https://cdn.discordapp.com/attachments/556951502137065518/900806259228422154/drifterFace.PNG"
+            style="border-radius: 50%; height: 96px; width: 96px; border: solid 0.01em black"
+          ></v-img>
+        </div>
+        <div class="ml-4" style="display: flex; flex-direction: column; align-items: start">
+          <div style="display: flex; flex-direction: row; align-items: end">
+            <h1 style="margin-bottom: -8px">Maxime</h1>
+            <span class="text-grey-darken-3 ml-2">@mdolr</span>
+          </div>
+          <div
+            class="account-stats"
+            style="margin-left: -12px; display: flex; flex-direction: row; align-items: center; justify-content: start"
+          >
+            <span><v-icon small>mdi-account-group</v-icon> 127 000 Followers</span>
+            <span><v-icon x-small>mdi-camera</v-icon> 323 Pictures</span>
+            <span class="text-purple"
+              ><v-icon x-large color="purple">mdi-calendar-multiple-check</v-icon> <strong>PERFECT!</strong></span
+            >
+          </div>
+          <p style="text-align: left; margin-top: 6px">
+            Mon super profil avec une description multi-ligne de folie que tout le monde adore lire avec une limite de
+            280 caractères???
+          </p>
+        </div>
+      </div>
     </div>
+
     <br />
     <div class="account-buttons">
       <v-btn><v-icon>mdi-account-plus</v-icon>Follow</v-btn>
@@ -36,7 +52,7 @@
 
 <style scoped>
 .account-stats span {
-  margin-top: 4px;
+  margin-left: 12px;
   display: inline-block;
 }
 
