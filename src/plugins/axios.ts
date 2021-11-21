bc6de130c37d3784ce86346c3f703580fd200291
Maxime Dolores <19026937+mdolr@users.noreply.github.com>
2021-11-21 01:35:12 +0100

    fix: fix base_url not loading properly

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.BASE_URL,
+  baseURL:
+    process.env.NODE_ENV == 'production'
+      ? 'https://tinyinsta-web.oa.r.appspot.com/_ah/api/tinyinsta/v1'
+      : 'http://localhost:8080/_ah/api/tinyinsta/v1',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
